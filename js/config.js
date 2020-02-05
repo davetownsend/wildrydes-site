ed9bfb7090d1b62c779a502964700b95c0051397
davetownsend <dtownsend@matson.com>
2020-02-05 18:22:10 +0000

    added apigw endpoint

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_f93vZHapf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '38lejconjlivpa6ujagunle0d7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_f93vZHapf', 
+        userPoolClientId: '38lejconjlivpa6ujagunle0d7',
+        region: 'us-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://xmk5dlckxj.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
